d1cae1ccecdce542f7f4930903b9dc97b87d587e
schlaBAM <brett.morrison@live.com>
2017-09-28 15:50:17 -0700

    Initial Commit

---
+++ RandomImageCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomImageCollector
+//
+//  Created by BAM on 2017-09-28.
+//  Copyright © 2017 BAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
